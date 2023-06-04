c7e9259eca9111f15d6f74be197819b107277851
kjetilfr <kjefriborg@gmail.com>
2023-06-04 13:42:24 +0200

    removed file

--- index.py
+++ index.py
@@ -7,8 +7,20 @@ from selenium.webdriver.support.ui import Select
 import time
 import random
 
+test_ua = 'Mozilla/5.0 (Windows NT 4.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/37.0.2049.0 Safari/537.36'
 
-driver = webdriver.Chrome()
+options = Options()
+
+# options.add_argument("--headless")  # Remove this if you want to see the browser (Headless makes the chromedriver not have a GUI)
+options.add_argument("--window-size=1920,1080")
+
+options.add_argument(f'--user-agent={test_ua}')
+# options.binary_location = "C:\Program Files\Google\Chrome Beta\Application"
+chrome_driver_binary = "C:/chromedriver"
+options.add_argument('--no-sandbox')
+options.add_argument("--disable-extensions")
+
+driver = webdriver.Chrome(chrome_driver_binary, options=options)
 driver.get("http://www.nordicmafia.org")
 #driver.get('file:///C:/Users/Kjetil/Desktop/nm%20antibot.html')
 
@@ -201,7 +213,9 @@ def checkAntiBot():
     if len(driver.find_elements(By.XPATH, "//div[text()='Anti-bot']")) > 0:
         print("bot")
         time.sleep(sleepRandomLow()/2)
-        #Hidden anti-bot code
+        solver = RecaptchaSolver(driver=driver)
+        recaptcha_iframe = driver.find_element(By.XPATH, '//iframe[@title="reCAPTCHA"]')
+        solver.click_recaptcha_v2(iframe=recaptcha_iframe)
     else:
         print("no bot")
 
